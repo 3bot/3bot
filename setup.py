05e60c9f112963d944d3ea1d56af35fca344829a
Philippe O. Wagner <wagner@arteria.ch>
2014-08-29 23:21:32 +0200

    Reserved our space /placeholder

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup 
+
+setup(
+    name = "threebot",
+    version = "0.0.1",
+    author_email = "admin@arteria.ch",
+    maintainer_email = "admin@arteria.ch",
+    install_requires=[
+        "threebot-crypto",
+    ],
+)
