bdc8cb46d05cc2eb5b3e062504cb3660c79117c9
Walter Renner <wltr@posteo.is>
2015-07-28 16:34:30 +0200

    Update 0001_initial.py

--- threebot/migrations/0001_initial.py
+++ threebot/migrations/0001_initial.py
@@ -10,7 +10,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('organizations', '0002_auto_20150703_1129'),
+        ('organizations', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
