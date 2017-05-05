117f09427c81bb09c379d5ef1ccfcf6f5d72d827
Walter Renner <renner@arteria.ch>
2017-05-05 17:57:43 +0200

    first changes for django 1.11 [wip]

---
+++ tests/test_settings.py
@@ -0,0 +1,47 @@
+"""Settings that need to be set in order to run the tests."""
+import os
+
+DEBUG = True
+
+SITE_ID = 1
+
+SECRET_KEY = 'fake-key'
+
+APP_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'tests.urls'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(APP_ROOT, '../app_static')
+MEDIA_ROOT = os.path.join(APP_ROOT, '../app_media')
+STATICFILES_DIRS = (
+    os.path.join(APP_ROOT, 'static'),
+)
+
+EXTERNAL_APPS = (
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+)
+
+INTERNAL_APPS = (
+    'background_task',
+    'organizations',
+    'threebot',
+)
+
+INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
---
+++ threebot/views/base.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render_to_response, redirect
+from django.http import HttpResponseRedirect
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
+from django.template import RequestContext
+from django.conf import settings
+
+from organizations.models import Organization
+
+from threebot.models import Workflow
+from threebot.utils import get_my_orgs, filter_workflow_log_history
+
+
+@login_required
+def orgswitcher(request, slug):
+    org = Organization.objects.get(users=request.user, slug=slug)
+    request.session['currOrg'] = org
+    request.session.modified = True
+    return HttpResponseRedirect("/")  # FIXME: correct path, lang
+
+
+@login_required
+def chooseorg(request, template='threebot/chooseorg.html'):
+    orgs = Organization.objects.filter(users=request.user)
+
+    return render_to_response(template, {'request': request,
+                                         'orgs': orgs,
+                                        }, context_instance=RequestContext(request))
+
+
+def user_login(request, template='threebot/login.html'):
+    next = request.GET.get("next", getattr(settings, "LOGIN_REDIRECT_URL", "/"))
+    auth_form = AuthenticationForm(None, request.POST or None)
+
+    if request.user.is_authenticated():
+        # no need to login again, just redirect
+        return redirect(next)
+
+    if auth_form.is_valid():
+        # The form itself handles authentication and checking to make sure passowrd and such are supplied.
+        next = request.POST.get("next", getattr(settings, "LOGIN_REDIRECT_URL", "/"))
+        login(request, auth_form.get_user())
+        return HttpResponseRedirect(next)
+
+    return render_to_response(template, {'request': request,
+                                         'auth_form': auth_form,
+                                        }, context_instance=RequestContext(request))
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("/")
+
+
+@login_required
+def index(request, template='threebot/index.html'):
+    orgs = get_my_orgs(request)
+    team_logs = filter_workflow_log_history(teams=orgs, quantity=10)
+    return render_to_response(template, {'request': request,
+                                         'team_logs': team_logs,
+                                        }, context_instance=RequestContext(request))
